73d76fdf30bd336b2a10da36c9c24f85525a16d3
Alexander Trousevich <trousev@yandex.ru>
2013-03-08 13:38:37 +0400

    lost files

--- imageloader.js
+++ imageloader.js
@@ -19,12 +19,18 @@
           $(design).appendTo(___root_selector+" #images");
           
         }
-        $(___root_selector).append(
-             '<div id="images"></div>  '
-            +'<div id="images_new"></div>'
-            +'<input  type="file" id="on_append_image" name="file0"></input>'
-            +'<div class="imageloader-hidden" id="images_hidden"></div>'
-            );
+        var createinputfile = function(id)
+        {
+            ans = $('<div class="imageloader-external" id="ext_'+id+'"  style="background: url(add.png) no-repeat center center; cursor: pointer; text-align: left; vertical-align:top; overflow: hidden; width: 120px; height: 200px; border: 1px dotted #031a8f; ">'
+             +    '<div style="color: #fff; font-size: 2px; font-weight: bold;"></div>'
+             +    '<input id="on_append_image" type="file" name="'+id+'" size="1" style="cursor: pointer; margin-top: -1px; margin-left:-410px; -moz-opacity: 0; filter: alpha(opacity=0); opacity: 0; font-size: 320px; height: 200px;">'
+             +'</div>');
+            return ans;
+        }
+        $(___root_selector).append('<div id="images"></div>  ');
+        $(___root_selector).append('<div id="images_new"></div>');
+        $(___root_selector).append('<div class="imageloader-hidden" id="images_hidden"></div>');
+
         $(___root_selector+" img").each(function()
         {
           exfid = 'ex_'+exid();
@@ -34,6 +40,8 @@
           $(this).attr("class","imageloader-image");
           $(___root_selector+" #im_"+exfid+ " #image").append($(this));
         });
+        createinputfile('file0').appendTo(___root_selector+" #images");
+
         var fileid = function()
         {
             ___fileid += 1;
@@ -60,20 +68,28 @@
                     name: 'file',
                 });
                 $('#images').append(input);*/
-                oldfid = $(this).attr("name");
-                $(this).attr("id",oldfid);
-                $(this).appendTo($(___root_selector+" #images_hidden"));
+                me = $(this);
+                if($(this).attr('id').indexOf("ext_") !== -1) 
+                {
+                    me = $(this).find('input');
+                }
+                oldfid = me.attr("name");
+                me.attr("id",oldfid);
+                $("#ext_"+oldfid).appendTo($(___root_selector+" #images_hidden"));
                 fid = 'file'+fileid();
-                new_me = $("<input>", {
+                /*new_me = $("<input>", {
                     type: 'file',
                     name: fid, 
-                });
-                new_me.appendTo(___root_selector+" #images_new");
+                });*/
+                //alert(oldfid+"=>"+fid);
+                new_me = createinputfile(fid);
                 new_me.change(on_new_clicked);
                 createimage(oldfid);
+                new_me.appendTo(___root_selector+" #images");
                 imagenewhandler(oldfid);
-                
-                var file = this.files[0];
+                //alert(this);
+                //alert(me[0]);
+                var file = me[0].files[0];
                 var reader = new FileReader();
                 reader.onload = function(e) {
                     $('<img>', {height: 200, class: "imageloader-image"}).attr('src', e.target.result).appendTo(___root_selector+' #im_'+oldfid + " #image");
